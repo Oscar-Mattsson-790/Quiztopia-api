6549c4960484cfb61d22c59701133434361283a4
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-09-25 16:48:07 +0200

    feat: auth

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const { sendError } = require("../responses");
+const jwt = require("jsonwebtoken");
+
+const validateToken = {
+  before: async (request) => {
+    try {
+      if (!request.event.headers || !request.event.headers.authorization) {
+        throw new Error("401 Unauthorized");
+      }
+
+      const token = request.event.headers.authorization.replace("Bearer ", "");
+      if (!token) throw new Error("401 Unauthorized");
+
+      const data = jwt.verify(token, "a1b1c1");
+      request.event.userId = data.id;
+    } catch (error) {
+      throw new Error("401 Unauthorized");
+    }
+  },
+  onError: async (request) => {
+    if (request.error.message === "401 Unauthorized") {
+      return sendError(401, "Unauthorized");
+    }
+
+    throw request.error;
+  },
+};
+
+module.exports = { validateToken };
