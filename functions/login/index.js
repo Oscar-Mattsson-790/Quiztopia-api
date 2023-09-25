11b939185fecdd3e1ca5c64331883bdc1eb27191
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-09-25 15:52:01 +0200

    feat: add login

---
+++ functions/login/index.js
@@ -0,0 +1,40 @@
+const { sendResponse, sendError } = require("../../responses/index");
+const { db } = require("../../services/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const middy = require("@middy/core");
+
+async function getUserByEmail(email) {
+  const result = await db
+    .query({
+      TableName: "Users",
+      IndexName: "EmailIndex",
+      KeyConditionExpression: "email = :email",
+      ExpressionAttributeValues: { ":email": email },
+    })
+    .promise();
+  return result.Items[0];
+}
+
+const handler = middy(async (event) => {
+  try {
+    const { email, password } = JSON.parse(event.body);
+
+    const user = await getUserByEmail(email);
+    if (!user) {
+      return sendError(404, "User not found");
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return sendError(401, "Invalid password");
+    }
+
+    const token = jwt.sign({ id: user.userId }, "a1b1c1");
+    return sendResponse(200, { token });
+  } catch (error) {
+    return sendError(500, error.message);
+  }
+});
+
+module.exports = { handler };
